1c27f472ebe5d453c1e993e9d3ec9e0596392e8a
Roarkiner <dujour.lenaic@gmail.com>
2025-03-25 19:22:57 +0100

    Fix cors

--- pixel-war-back/server.js
+++ pixel-war-back/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const apiRoutes = require("./routes/apiRoutes");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    exposedHeaders: ['Authorization'],
+    origin: '*'
+}));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
